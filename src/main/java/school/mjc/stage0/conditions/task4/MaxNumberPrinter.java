31c4b5c253753ac0b7f361f6280598246a6ddba7
nikitakorneichuk <korneichuk.nikita@gmail.com>
2023-06-30 16:19:28 +0300

    Add some code

--- src/main/java/school/mjc/stage0/conditions/task4/MaxNumberPrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task4/MaxNumberPrinter.java
@@ -2,5 +2,16 @@ package school.mjc.stage0.conditions.task4;
 
 public class MaxNumberPrinter {
     public void printGreatest(int first, int second, int third) {
+        int max;
+       if (first <= second) {
+           if (second <= third) {
+               max = third;
+           } else {
+               max = second;
+           }
+       } else {
+           max = first;
+       }
+        System.out.println(max);
     }
 }
--- src/main/java/school/mjc/stage0/conditions/task4/TernaryMaxNumberPrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task4/TernaryMaxNumberPrinter.java
@@ -2,5 +2,12 @@ package school.mjc.stage0.conditions.task4;
 
 public class TernaryMaxNumberPrinter {
     public void printGreatest(int first, int second, int third) {
+        int max;
+        max =  first <= second ?
+                second <= third ?
+                        third:
+                        second:
+                first;
+        System.out.println(max);
     }
 }
